173630deb7142e038e7705616e29aa81e75d4e0f
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-30 23:29:46 -0800

    Question 11 completed

--- Q11.js
+++ Q11.js
@@ -2,10 +2,12 @@
 //Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time
 Object.defineProperty(exports, "__esModule", { value: true });
 let names = ["asad", "khalid", "ali", "arif"];
+//get array elements by uing index
 console.log(names[0]);
 console.log(names[1]);
 console.log(names[2]);
 console.log(names[3]);
-// names.forEach((Element) => {
-//   console.log(Element);
-// });
+//get array elements by using forEach() method
+names.forEach((Element) => {
+    console.log(Element);
+});

29d5080c5fd0a0e0ea0871a3c1d31d58814e0a93
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-31 08:01:28 -0800

    Question 23 completed

---
+++ Q23.js
@@ -0,0 +1,29 @@
+"use strict";
+//  Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+Object.defineProperty(exports, "__esModule", { value: true });
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let car = "subaru";
+let car1 = "honda";
+let car2 = "mercedes";
+let car13 = "hyundai";
+let car4 = "suzuki";
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == "subaru");
+console.log("Is car == 'audi'? I predict False.");
+console.log(car == "audi");
+console.log("Is car1 == 'honda'? I predict True.");
+console.log(car1 == "honda");
+console.log("Is car == 'toyota'? I predict False.");
+console.log(car == "toyota");
+console.log("Is car == 'suzuki'? I predict True.");
+console.log(car4 == "suzuki");
+console.log("Is car == 'mercedes'? I predict True.");
+console.log(car2 == "mercedes");
+console.log("Is car == 'bmw'? I predict False.");
+console.log(car == "bmw");
+console.log("Is car == 'hyundai'? I predict True.");
+console.log(car13 == "hyundai");

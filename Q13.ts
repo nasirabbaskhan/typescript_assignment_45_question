75d2b92f7e5f08f79636f7168506d682807e3c2b
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-30 23:48:46 -0800

    Question 13 completed

---
+++ Q13.ts
@@ -0,0 +1,22 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+let my_fav_mode = "motorcycle";
+let myownArray = [
+  "Honda motorcycle",
+  "CD motorcycle",
+  "125 Honda motorcycle",
+  "Yamma Honda motorcycle",
+];
+
+console.log(
+  `I would like to own a ${myownArray[0]} because my favorite mode is ${my_fav_mode}`
+);
+console.log(
+  `I would like to own a ${myownArray[1]} because my favorite mode is ${my_fav_mode}`
+);
+console.log(
+  `I would like to own a ${myownArray[2]} because my favorite mode is ${my_fav_mode}`
+);
+console.log(
+  `I would like to own a ${myownArray[3]} because my favorite mode is ${my_fav_mode}`
+);

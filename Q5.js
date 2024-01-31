693326d2c5375bb3f60cf389236250d424b16a4a
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-30 22:03:05 -0800

    question 5 completed

---
+++ Q5.js
@@ -0,0 +1,6 @@
+"use strict";
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+Object.defineProperty(exports, "__esModule", { value: true });
+let famous_person = "Ibn - e - Batuta";
+let message = "The world is a book, and those who do not travel read only a page.";
+console.log(`${famous_person} once said , "${message}"`);
---
+++ Q5.ts
@@ -0,0 +1,6 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person: string = "Ibn - e - Batuta";
+let message: string =
+  "The world is a book, and those who do not travel read only a page.";
+console.log(`${famous_person} once said , "${message}"`);

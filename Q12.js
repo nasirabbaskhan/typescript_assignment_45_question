3722cb30a0b08c5169e54d5a07ddd315c48f3459
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-30 23:29:01 -0800

    Question 12 completed

---
+++ Q12.js
@@ -0,0 +1,9 @@
+"use strict";
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+Object.defineProperty(exports, "__esModule", { value: true });
+let names = ["Asad", "Khalid", "Ali", "Arif"];
+let message = "Hello, How are you?";
+console.log(`${message} ${names[0]}!`);
+console.log(`${message} ${names[1]}!`);
+console.log(`${message} ${names[2]}!`);
+console.log(`${message} ${names[3]}!`);

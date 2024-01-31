9aefebda57fe577b90a67618e4475a85512e9115
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-31 00:09:02 -0800

    Question 16 completed

---
+++ Q15.js
@@ -0,0 +1,10 @@
+"use strict";
+//Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+// it uses a `for` loop to iterate over
+// the `guest_name` array and print a message for each guest, inviting them to the dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+let guest_name = ["Ali", "Ahmad", "Nasir", "Amir", "Sadiq", "kashif"];
+for (let i = 0; i < guest_name.length; i++) {
+    let guestName = guest_name[i];
+    console.log(`Hello ${guestName}! I invited you to come on dinner.\nThank You\n`);
+}

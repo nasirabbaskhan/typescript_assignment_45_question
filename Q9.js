7f7adfd8de4a064da8531bc07d881d3a8e05ce2c
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-30 22:32:34 -0800

    Question 9 completed

---
+++ Q9.js
@@ -0,0 +1,6 @@
+"use strict";
+//  Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message
+Object.defineProperty(exports, "__esModule", { value: true });
+let myFavoriteNumber = 12;
+let message = `my favorite number is ${myFavoriteNumber} :`;
+console.log(message);

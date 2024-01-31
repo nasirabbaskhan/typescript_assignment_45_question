888f5b73e887de44277f043e4b22632f0f05832a
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-31 10:03:19 -0800

    Question 36 completed

---
+++ Q36.js
@@ -0,0 +1,8 @@
+"use strict";
+//T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+Object.defineProperty(exports, "__esModule", { value: true });
+function make_shirt(size, text) {
+    console.log("the size of shirt is:", size);
+    console.log("the text on the shirt is:", text);
+}
+make_shirt(23, "I love cooding");

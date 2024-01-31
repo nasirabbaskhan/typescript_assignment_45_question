04ba0e0e27d39d12dbb25166d76fde7f307bb613
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-30 22:44:54 -0800

    Question 7 completed

---
+++ Q7.js
@@ -0,0 +1,11 @@
+"use strict";
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+Object.defineProperty(exports, "__esModule", { value: true });
+let addition = 4 + 4;
+let substruction = 12 - 4;
+let multiplication = 2 * 4;
+let division = 16 / 2;
+console.log("Addition :" + addition);
+console.log("Subtraction :" + substruction);
+console.log("Multiplication :" + multiplication);
+console.log("Division :" + division);

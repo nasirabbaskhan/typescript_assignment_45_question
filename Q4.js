4e1c5b1b8d35f56575b62dea2f375073d2424444
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-30 21:57:04 -0800

    Question 4 completed

---
+++ Q4.js
@@ -0,0 +1,11 @@
+"use strict";
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+Object.defineProperty(exports, "__esModule", { value: true });
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+let Quote = "A person who never made a mistake never tried anything new.";
+let auther = "Albert Einstein";
+console.log(`${auther} once said , "${Quote}"`);
+// a famous quate of famous person
+let famousQuote = "The world is a book, and those who do not travel read only a page..";
+let famousAuther = "Ibn - e - Batuta";
+console.log(`${famousAuther} once said , "${famousQuote}"`);
---
+++ Q4.ts
@@ -0,0 +1,16 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+let Quote: string =
+  "A person who never made a mistake never tried anything new.";
+let auther: string = "Albert Einstein";
+
+console.log(`${auther} once said , "${Quote}"`);
+
+// a famous quate of famous person
+let famousQuote: string =
+  "The world is a book, and those who do not travel read only a page.";
+let famousAuther: string = "Ibn - e - Batuta";
+
+console.log(`${famousAuther} once said , "${famousQuote}"`);

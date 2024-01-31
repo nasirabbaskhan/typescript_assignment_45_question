1f590225ef62384779d89931db1b6b5fdbd1abe1
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-31 10:48:18 -0800

    Question 41 completed

---
+++ Q41.ts
@@ -0,0 +1,10 @@
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+
+let magicians = ["David Copperfield", "David Blaine", "Criss Angel"];
+console.log("The name of magicians:");
+function show_magicians(magician: string[]) {
+  magician.forEach((element) => {
+    console.log(element);
+  });
+}
+show_magicians(magicians);

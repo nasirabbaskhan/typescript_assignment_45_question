6e81606bfbc5c3cd393566ceff9f1bc9e64eb386
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-01-30 23:16:45 -0800

    Question 11 completed

---
+++ Q11.js
@@ -0,0 +1,11 @@
+"use strict";
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time
+Object.defineProperty(exports, "__esModule", { value: true });
+let names = ["asad", "khalid", "ali", "arif"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+// names.forEach((Element) => {
+//   console.log(Element);
+// });
---
+++ Q11.ts
@@ -0,0 +1,13 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time
+
+let names = ["asad", "khalid", "ali", "arif"];
+//get array elements by uing index
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+
+//get array elements by using forEach() method
+names.forEach((Element) => {
+  console.log(Element);
+});

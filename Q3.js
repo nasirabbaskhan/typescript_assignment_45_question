56dd36cbb08ca9336c13883c231411b1efddca8f
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-02-01 23:52:57 -0800

    Question 3 completed

---
+++ Q3.js
@@ -0,0 +1,8 @@
+"use strict";
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("performing name cases:");
+let personName = "Nasir Abbas";
+let personlowercaseName = personName.toLowerCase();
+let persontoUpperCaseName = personName.toUpperCase();
+let persontitlecaseName = console.log(`${personName},${personlowercaseName}`);
---
+++ Q3.ts
@@ -0,0 +1,9 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+console.log("performing name cases:");
+
+let personName = "Nasir Abbas";
+let personlowercaseName = personName.toLowerCase();
+let persontoUpperCaseName = personName.toUpperCase();
+
+let persontitlecaseName = console.log(`${personName},${personlowercaseName}`);
